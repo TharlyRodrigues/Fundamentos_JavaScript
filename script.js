157d20a243b67183fb9c5abdff914f578a0a02e6
TharlyRodrigues <tharly.portifolio@gmail.com>
2023-12-21 10:13:00 -0300

    update

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+/*
 let js = "amazing";
 console.log(40 + 8 + 23 - 10);
 
@@ -8,3 +9,29 @@ let firstName = "tharly";
 console.log(firstName);
 console.log(firstName);
 console.log(firstName);
+
+let javaScriptIsFun = true;
+console.log(javaScriptIsFun);
+
+// console.log(typeof true);
+console.log(typeof javaScriptIsFun);
+// console.log(typeof 23);
+// console.log(typeof "Tharly");
+
+javaScriptIsFun = "Yes!";
+console.log(typeof javaScriptIsFun);
+
+// identificando valores nulos
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1995;
+console.log(typeof year);
+
+// bug
+console.log(typeof null);
+*/
+
+let age = 30;
+age = 31;
