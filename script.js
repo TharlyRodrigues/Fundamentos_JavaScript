76b5c488c7b12730df1b79dbdf88e2af2effc7b4
TharlyRodrigues <tharly.portifolio@gmail.com>
2023-12-21 09:36:32 -0300

    javaScript_Fundamento

---
+++ script.js
@@ -0,0 +1,10 @@
+let js = "amazing";
+console.log(40 + 8 + 23 - 10);
+
+console.log("tharly e tamires");
+console.log(23);
+
+let firstName = "tharly";
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
