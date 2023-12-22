b711f71fa2ea15f5394a214886c3d84589344051
TharlyRodrigues <tharly.portifolio@gmail.com>
2023-12-22 18:11:56 -0300

    update

--- script.js
+++ script.js
@@ -92,4 +92,26 @@ console.log(x, y);
 const averageAge = ageTharly + ageTamires / 2;
 console.log(ageTharly, ageTamires, averageAge);
 
+
+
+const firstName = "Jonas";
+const job = "teacher";
+const birthYear = 1991;
+const year = 2037;
+
+const jonas =
+  "I'm " + firstName + ", a " + (year - birthYear) + " years old " + job + "!";
+console.log(jonas);
+
+const jonasNew = `i'm ${firstName}, a ${year - birthYear} year old ${job}!`;
+console.log(jonasNew);
+console.log(`Just a regular string...`);
+
+console.log(`string with \n\
+multiple \n\
+lines`);
+
+console.log(`string
+multiple
+lines`);
 */
