b9140a12de4f55e7c37244a06b5ea4faeccd6b13
TharlyRodrigues <tharly.portifolio@gmail.com>
2023-12-26 20:49:02 -0300

    update

--- script.js
+++ script.js
@@ -228,4 +228,45 @@ if (hasDriversLicense && hasGoodVision && !isTired) {
 // console.log(isTrue && isFalse); // false (true se ambos são true)
 // console.log(isTrue || isFalse); // true (true se pelo menos um é true)
 // console.log(!isTrue);            // false (negação lógica)
+
+
+const day = "friday";
+
+switch (day) {
+  case "monday": // day === 'monday'
+    console.log("plan course structure");
+    console.log("Go to coding meetup");
+    break;
+  case "tuesday":
+    console.log("Prepare theory vides");
+    break;
+  case "wednesday":
+  case "thursday":
+    console.log("Write code examples");
+    break;
+  case "friday":
+    console.log("Record videos");
+    break;
+  case "saturday":
+  case "sunday":
+    console.log("enjoy the weekend :D");
+    break;
+  default:
+    console.log("Not a valid day");
+}
+
+if (day === "monday") {
+  console.log("plan course structure");
+  console.log("Go to coding meetup");
+} else if (day === "tuesday") {
+  console.log("Prepare theory vides");
+} else if (day === "wednesday" || day === "thursday") {
+  console.log("Write code examples");
+} else if (day === "friday") {
+  console.log("Record videos");
+} else if (day === "saturday" || day === "sunday") {
+  console.log("enjoy the weekend :D");
+} else {
+  console.log("Not a valid day!");
+}
 */
